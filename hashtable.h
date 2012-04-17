c901f2c41ba095d584d4c2da25c30c23708abbd3
Emil Taylor Bye <phptph@gmail.com>
2012-04-17 14:14:01 +0200

    Added basic function outline

---
+++ hashtable.h
@@ -0,0 +1,24 @@
+#ifndef HASTABLE_H_
+#define HASHTABLE_H_
+
+#define HASHTABLE_DEFAULT_SIZE 128
+
+typedef struct _ht_list {
+	char *string;
+	struct _ht_list *next;
+} ht_list;
+
+typedef struct _hash_table {
+	int size;
+	ht_list ** elements;
+} hash_table;
+
+unsigned int getHash(hash_table *, char *);
+hash_table *getNewHashTable(int);
+int hasElement(hash_table *, char *);
+ht_list *getElement(hash_table *, char *);
+ht_list *addElement(hash_table *, char *);
+void removeElement(hash_table *, char *);
+void freeTable(hash_table *);
+
+#endif /*HASHTABLE_H_*/
