f16f32a5bc27382e48d9a684d5262a9b6f83aa24
Emil Taylor Bye <phptph@gmail.com>
2012-04-17 14:29:36 +0200

    Added the hashtable.c-file and added a hash function

---
+++ hashtable.c
@@ -0,0 +1,12 @@
+#include "hashtable.h"
+
+unsigned int getHash(hash_table *hashTable, char *string) {
+	unsigned int hashValue = 0;
+
+	while (*string != '\0') {
+		hashValue = *string + (hashValue << 5) - hashValue;
+		string++;
+	}
+
+	return hashValue % hashTable->size;
+}
