7380813832d790b6546f69ca6c2c52471dcb50c7
Emil Taylor Bye <phptph@gmail.com>
2012-05-02 21:27:43 +0200

    Removed default size- and get_hash_table macros from hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -1,7 +1,6 @@
 #ifndef HASTABLE_H_
 #define HASHTABLE_H_
 
-#define HASH_TABLE_DEFAULT_SIZE 128
 #define HASH_TABLE_GROW_SIZE 0.75
 
 typedef struct _hash_table_list {
@@ -20,7 +19,6 @@ typedef struct _hash_table {
 unsigned long get_hash(char *);
 unsigned int get_hash_table_position(hash_table_t *, char *);
 hash_table_t *get_hash_table(int);
-#define get_hash_table() get_hash_table(HASH_TABLE_DEFAULT_SIZE)
 int has_hash_table_element(hash_table_t *, char *);
 hash_table_list_t *get_hash_table_element(hash_table_t *, char *);
 hash_table_list_t *add_hash_table_element(hash_table_t *, char *);
